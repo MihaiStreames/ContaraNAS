e1e81d71511696a73ae3f56af1fefcd3bc447bdf
Mihai Bors <moreliciouscod@gmail.com>
2025-11-25 16:05:27 +0100

    Update main.py

--- ContaraNAS/main.py
+++ ContaraNAS/main.py
@@ -1,12 +1,8 @@
 import asyncio
 import os
 
-from nicegui import app, ui
-
 from ContaraNAS.core.module_manager import ModuleManager
 from ContaraNAS.core.utils import get_logger
-from ContaraNAS.gui.factories import register_all_components
-
 
 # Some fixes
 os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --no-sandbox"
@@ -24,10 +20,8 @@ def setup_gui(manager: ModuleManager):
     """Setup the main GUI application"""
     logger.info("Setting up GUI...")
 
-    ui.colors(primary="#1976d2")
-
     # Restore module states after UI is ready (using timer for native mode)
-    ui.timer(0.1, lambda: asyncio.create_task(restore_module_states(manager)), once=True)
+    # ui.timer(0.1, lambda: asyncio.create_task(restore_module_states(manager)), once=True)
 
     logger.info("GUI setup complete")
 
@@ -43,21 +37,11 @@ def main():
     logger.info("Starting ContaraNAS...")
 
     # Setup components
-    register_all_components()
     manager = ModuleManager()
     setup_gui(manager=manager)
 
     # Configure app shutdown handler
-    app.on_shutdown(lambda: asyncio.create_task(cleanup_on_shutdown(manager)))
-
-    # Run the application
-    ui.run(
-        title="ContaraNAS",
-        native=True,
-        window_size=(1200, 800),
-        fullscreen=False,
-        reload=False,
-    )
+    # app.on_shutdown(lambda: asyncio.create_task(cleanup_on_shutdown(manager)))
 
 
 if __name__ == "__main__":
